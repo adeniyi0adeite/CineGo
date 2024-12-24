21c217cd4d3c17f0b419c489688d7f2725db300a
adeniyi0adeite <adeniyi0adeite@gmail.com>
2024-12-24 16:01:35 +0100

    generate requirements

--- cinego/settings.py
+++ cinego/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -132,8 +132,11 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
 # For serving static files in development
+
 STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / 'static'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+
 
 # For media files (user-uploaded files)
 MEDIA_URL = '/media/'
